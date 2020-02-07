bcc1e84efb3142411f1f39e0318584c918e97146
Nicolas Geoffray <ngeoffray@google.com>
2020-02-07 14:02:18 +0000

    sdm660-common: Update in pinned files.
    Remove or update paths that don't exist.
    
    Bug: 119800099
    Test: boots
    Change-Id: Ib2cea88499cf08274d620d9d85ffcafc31fee868
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>
    Signed-off-by: orgesified <gesishabani1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,9 +27,6 @@ DEVICE_PATH := device/xiaomi/whyred
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
-# APEX image
-DEXPREOPT_GENERATE_APEX_IMAGE := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
