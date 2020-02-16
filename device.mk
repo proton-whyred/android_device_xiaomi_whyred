4c3898f6b60465b13b5ebd00aec335342d8f8ede
Subhajeet Muhuri <kenny3fcb@gmail.com>
2020-02-16 10:36:37 +0530

    whyred: AOSiP bringup

--- device.mk
+++ device.mk
@@ -65,8 +65,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay
 
 # Ramdisk
 PRODUCT_PACKAGES += \
--- extract-files.sh
+++ extract-files.sh
@@ -26,7 +26,7 @@ export DEVICE_COMMON=sdm660-common
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
-DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+AOSiP_ROOT="$MY_DIR"/../../..
+DEVICE_BLOB_ROOT="$AOSiP_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
 patchelf --add-needed libcamera_sdm660_shim.so "$DEVICE_BLOB_ROOT"/vendor/lib/hw/camera.sdm660.so
