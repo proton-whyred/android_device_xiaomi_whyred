12781ce5ccb133b3be310d62614495ab9a7bce9f
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    whyred: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde

--- device.mk
+++ device.mk
@@ -468,11 +468,6 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm660
-
-PRODUCT_PACKAGES += \
     XiaomiParts
 
 # USB
