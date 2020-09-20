75f9b810b4a1e15f2ab06ff7b421b736274b7efc
orgesified <gesishabani1@gmail.com>
2020-09-20 15:18:20 +0200

    Revert "whyred: Import and copy kernel headers to the build tree"
    This reverts commit cc13f873b3b9b7c81daa1db6f980369625328437.

--- AndroidBoard.mk
+++
@@ -1,6 +0,0 @@
-DEVICE_PATH := device/xiaomi/whyred
-
-# copy kernel headers to the build tree
-$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard $(DEVICE_PATH)/headers/*)
-	rm -rf $@
-	cp -a $(DEVICE_PATH)/headers/. $@
