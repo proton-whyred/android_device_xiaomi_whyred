b23323eedf89c4d7e9a2513d19de615e91f2b980
orgesified <me@orgesified.ml>
2020-11-06 22:20:03 +0100

    whyred: switch to redfin november fp
    Signed-off-by: orgesified <me@orgesified.ml>
    Change-Id: Ia8471ceb3d05d10f2a7dddb401c3f607f0941924

--- pa_whyred.mk
+++ pa_whyred.mk
@@ -56,10 +56,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="whyred" \
     PRODUCT_NAME="whyred" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RD1A.200810.020 6792687 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RD1A.201105.003.C1 6886399 release-keys"
 
 #Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RD1A.200810.020/6792687:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
