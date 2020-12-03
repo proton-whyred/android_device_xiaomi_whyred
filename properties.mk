1b7bc2500c087d4fcd1627d2b6603c644b78e26d
Volodymyr Zhdanov <wight554@gmail.com>
2020-12-03 23:42:51 +0000

    Revert "whyred: Use phase offsets as duration"
    This reverts commit a1bb509e906c24335a4b382e0463f622038e0aba.
    
    * seems to cause ANR during app starts leading to irresponsible system

--- properties.mk
+++ properties.mk
@@ -76,13 +76,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.disable_backpressure= 1 \
-    debug.sf.use_phase_offsets_as_durations=1 \
-    debug.sf.late.sf.duration=10500000 \
-    debug.sf.late.app.duration=20500000 \
-    debug.sf.early.sf.duration=16000000 \
-    debug.sf.early.app.duration=16500000 \
-    debug.sf.earlyGl.sf.duration=13500000 \
-    debug.sf.earlyGl.app.duration=21000000 \
+    debug.sf.early_app_phase_offset_ns=11600000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.early_phase_offset_ns=11600000 \
+    debug.sf.phase_offset_threshold_for_next_vsync_ns=11600000 \
     debug.sf.enable_gl_backpressure=1 \
     debug.cpurend.vsync=false \
     debug.hwui.use_buffer_age=false \
@@ -97,6 +95,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     video.disable.ubwc=1 \
     ro.sf.lcd_density=440 \
     ro.surface_flinger.use_color_management=true
+
+# The default sf phase offset is set to 6ms, to avoid it be included into next
+# vsync threshold, set high fps early sf and next vsync threshold phase offset
+# to 6.1ms, which is bigger than all sf phase offsets in normal frame rate.
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.high_fps_early_phase_offset_ns=6100000 \
+    debug.sf.high_fps_early_gl_phase_offset_ns=9000000
 
 # Dual SIM
 PRODUCT_PROPERTY_OVERRIDES += \
