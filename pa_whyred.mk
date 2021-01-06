280a5fd8a7237cf0f067d12d6925699fe22e3a18
orges <me@orgesified.ml>
2021-01-06 12:38:11 +0000

    whyred: redfin jan 2021 fp
    Signed-off-by: orges <me@orgesified.ml>

--- pa_whyred.mk
+++ pa_whyred.mk
@@ -56,10 +56,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="whyred" \
     PRODUCT_NAME="whyred" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.201205.011 6966805 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210105.003 7005429 release-keys"
 
 #Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.201205.011/6966805:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
