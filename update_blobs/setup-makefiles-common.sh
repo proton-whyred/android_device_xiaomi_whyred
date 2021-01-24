97f44f8c7c26114ced3eaebda6a79ac6f0193b40
orges <me@orgesified.ml>
2021-01-24 17:19:35 +0000

    whyred: align extract tools with template
    Signed-off-by: orges <me@orgesified.ml>

--- update_blobs/setup-makefiles-common.sh
+++ update_blobs/setup-makefiles-common.sh
@@ -1,67 +1,48 @@
 #!/bin/bash
 #
-# Copyright (C) 2020 Paranoid Android
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-DEVICE_COMMON=sdm660-common
-GUARDED_DEVICES_COMMON="twolip jasmine_sprout wayne clover lavender platina jason whyred"
-VENDOR=xiaomi
-
-INITIAL_COPYRIGHT_YEAR=2018
-
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-if [[ -z "$DEVICE_DIR" ]]; then
-    DEVICE_DIR="${MY_DIR}/../../${DEVICE}"
-fi
+ANDROID_ROOT="${MY_DIR}/../../../.."
 
-ROOT="$MY_DIR"/../../../..
-
-HELPER="$ROOT"/vendor/pa/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
-# Initialize the common helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$ROOT" true
+# Initialize the helper for common
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
-if ([[ "$ONLY_DEVICE" = "false" ]] || [[ -z "$ONLY_DEVICE" ]]) && [[ -s "${MY_DIR}"/proprietary-files-common.txt ]]; then
-    # Copyright headers and guards
-    write_headers "$GUARDED_DEVICES_COMMON"
-    # The common blobs
-    write_makefiles "$MY_DIR"/proprietary-files-common.txt true
+# Warning headers and guards
+write_headers "whyred"
+
+# The standard common blobs
+write_makefiles "proprietary-files-common.txt" true
+
+# Finish
+write_footers
+
+if [ -s "proprietary-files.txt" ]; then
+    # Reinitialize the helper for device
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
+
+    # Warning headers and guards
+    write_headers
+
+    # The standard device blobs
+    write_makefiles "proprietary-files.txt" true
 
     # Finish
-    write_footers
-fi
-if ([[ "$ONLY_COMMON" = "false" ]] || [[ -z "$ONLY_COMMON" ]]) && [[ -s proprietary-files.txt ]]; then
-    # Reinitialize the helper for device and write copyright headers and guards
-    DEVICE_COMMON="$DEVICE"
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "$DEVICE" "$VENDOR" "$ROOT" "$IS_COMMON" "$CLEAN_VENDOR"
-    # Copyright headers and guards
-    write_headers "$GUARDED_DEVICES"
-    # The standard device blobs
-    write_makefiles proprietary-files.txt true
-    # We are done!
     write_footers
 fi
--- update_blobs/setup-makefiles.sh
+++ update_blobs/setup-makefiles.sh
@@ -19,7 +19,6 @@ set -e
 
 export DEVICE=whyred
 export VENDOR=xiaomi
-export DEVICE_BRINGUP_YEAR=2020
 export DEVICE_COMMON=sdm660-common
 
 ./setup-makefiles-common.sh $@
