a563dffe5b6b2495c7425c84ccb2a46c04803c59
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-02-25 20:31:33 +0530

    whyred: power-libperfmgr: Switch to AIDL Power HAL hardware/google/pixel/power-libperfmgr from android-11.0.0_r25
    SQUASHED:
    Revert all HIDL interface and nuke previous changes
    Import Pixel libperfmgr AIDL Power HAL
    Adapt and rebrand for xiaomi_sdm660
    Remove Google-specific display LPM control
    Remove Google-specific camera and audio hints
    Remove VR hints handling
    Remove audio hints handling
    Remove dumpstate support
    Initialize powerHAL when boot is completed
    Add support for tap-to-wake feature control
    Add sepolicy rules for power-libperfmgr
    Enable power-libperfmgr
    
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- device.mk
+++ device.mk
@@ -370,7 +370,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr
+    android.hardware.power-service.xiaomi_sdm660-libperfmgr
 
 # Hint
 PRODUCT_COPY_FILES += \
--- vndk/vndk-ext-libs.mk
+++ vndk/vndk-ext-libs.mk
@@ -1,2 +1,5 @@
 EXTRA_VENDOR_LIBRARIES := \
     libpowermanager
+
+EXTRA_SYSTEM_LIBRARIES_64 := \
+    libblasV8
