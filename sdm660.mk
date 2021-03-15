68c20b9db0fa59e16e041e1c4aa0eaaa8ec55153
OdSazib <odsazib@gmail.com>
2021-03-15 12:21:09 +0600

    whyred: Build power stats and label it

--- sdm660.mk
+++ sdm660.mk
@@ -328,7 +328,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi_sdm660-libperfmgr
+    android.hardware.power-service.xiaomi_sdm660-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 # Hint
 PRODUCT_COPY_FILES += \
