2f630e153760381ef406020336f4dcd07a8fc18b
orges <me@orgesified.ml>
2021-07-18 19:53:12 +0000

    Revert "Inherit base system_ext.mk files for aosp targets"
    This reverts commit 920d1880c1932b095178836486f0a35d15f13952.
    
     * inherited by aosp_base_telephony.mk
    
    Signed-off-by: orges <me@orgesified.ml>

--- whyred.mk
+++ whyred.mk
@@ -19,12 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-#
-# All components inherited here go to system_ext image
-#
-$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
-
 # No Prebuilt GMS please.
 WITH_GMS := false
 
