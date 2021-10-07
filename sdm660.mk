52f89c54ff07e220820b1e71a63bed93181a7dcc
maitreya29 <maitreyapatni30@gmail.com>
2021-10-08 01:06:47 +0200

    sdm660-common: Build android.hardware.radio.deprecated
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.radio.deprecated@1.0.so" not found

--- sdm660.mk
+++ sdm660.mk
@@ -364,6 +364,11 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.radio@1.5 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.1 \
     android.hardware.secure_element@1.0 \
     android.hardware.secure_element@1.2 \
     android.hardware.secure_element@1.2.vendor \
