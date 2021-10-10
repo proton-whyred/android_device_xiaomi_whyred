34a04d5708a8fafe7628d8e93257d785e93116d6
Danny Lin <danny@kdrag0n.dev>
2021-10-09 17:04:45 -0700

    whyred: Override build fingerprint from redfin SP1A.210812.015
    Change-Id: I8b971dcefa3087ed1e81ec634199bc2c5a3ff27b

--- whyred.mk
+++ whyred.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_BRAND := Xiaomi
+PRODUCT_BRAND := xiaomi
 PRODUCT_DEVICE := whyred
-PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_NAME := whyred
 PRODUCT_MODEL := Redmi Note 5
 
@@ -22,7 +22,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
-
-BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
+PRODUCT_OVERRIDE_FINGERPRINT := google/redfin/redfin:12/SP1A.210812.015/7679548:user/release-keys
