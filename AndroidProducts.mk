f8896ddc2960995255ee44d629b9edc2028b6c7f
orges <me@orgesified.ml>
2021-10-11 19:16:03 +0000

    whyred: aosp bringup
    Signed-off-by: orges <me@orgesified.ml>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_whyred.mk
+    $(LOCAL_DIR)/whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_whyred-user \
-    lineage_whyred-userdebug \
-    lineage_whyred-eng
+    whyred-user \
+    whyred-userdebug \
+    whyred-eng
