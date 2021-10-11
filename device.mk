f650ce7ede0e27f709cbc1c81b86842883ed4a6a
orges <me@orgesified.ml>
2021-10-11 19:16:19 +0000

    whyred: build gcam go
    Signed-off-by: orges <me@orgesified.ml>

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GCam
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # HW crypto
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
